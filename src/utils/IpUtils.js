4f5b582404826dceeafa4b5a57abcb67805eae4e
Nuno Alves <nuno.alves02@gmail.com>
2020-11-01 13:26:24 +0000

    Add dinamic IP to images and reports

--- src/utils/IpUtils.js
+++ src/utils/IpUtils.js
@@ -1,12 +1,16 @@
 module.exports = {
   getImagesAddress(filename) {
-    const url = filename ? 'http://localhost:8085/images/' + filename : null;
+    const url = filename
+      ? (process.env.IMAGES_URL || 'http://localhost:8085/images/') + filename
+      : null;
 
     return url;
   },
 
   getReportsAddress(filename) {
-    const url = filename ? 'http://localhost:8085/reports/' + filename : null;
+    const url = filename
+      ? (process.env.REPORTS_URL || 'http://localhost:8085/reports/') + filename
+      : null;
 
     return url;
   },
