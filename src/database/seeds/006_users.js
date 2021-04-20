b49e90d3f023f79043c4eada48efcc0eefcdda19
Nuno Alves <nuno.alves02@gmail.com>
2021-04-21 00:18:57 +0100

    Update seed

--- src/database/seeds/006_users.js
+++ src/database/seeds/006_users.js
@@ -1,26 +1,30 @@
-const { encript } = require('../../utils/PasswordUtils');
-const { generateId } = require('../../utils/UserUtils');
-const EnumCharges = require('../../utils/enums/EnumCharges');
+const { encript } = require('../../utils/PasswordUtils')
+const { generateId } = require('../../utils/UserUtils')
+const EnumCharges = require('../../utils/enums/EnumCharges')
 
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  const id = await generateId();
-  const password = await encript('admin');
+  const id = await generateId()
+  const password = await encript('admin')
 
-  return knex('accounts').del().then(async () => knex('users')
-    .del())
+  return knex('accounts')
+    .del()
+    .then(async () => knex('users').del())
     .then(async () =>
       // Inserts seed entries
       knex('users').insert({
         id,
         username: 'Admin',
-        email: 'admin@splitbook.com',
+        email: 'admin@splitbook.pt',
         email_confirmed: true,
-        password,
-      }))
-    .then(async () => knex('accounts').insert({
-      user_id: id,
-      name: 'Administrador',
-      charge: EnumCharges.ADMIN.charge,
-    }));
-};
+        password
+      })
+    )
+    .then(async () =>
+      knex('accounts').insert({
+        user_id: id,
+        name: 'Administrador',
+        charge: EnumCharges.ADMIN.charge
+      })
+    )
+}
